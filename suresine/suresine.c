4ee57c01cd0c14f6e97c3df59e6a950bb2890d06
Mark Swisher <mswishe7@mix.wvu.edu>
2015-12-02 01:28:14 -0500

    Update suresine.c

--- suresine/suresine.c
+++ suresine/suresine.c
@@ -2,23 +2,6 @@
  *  suresine.c - This program reads all the RAM and EEPROM registers on a Moringstar SureSine-300 and prints the results.
  *  
 
-Copyright 2012 Tom Rinehart.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
 /* Compile with: cc `pkg-config --cflags --libs libmodbus` suresine.c -o suresine */
 
 #include <stdio.h>
@@ -323,7 +306,6 @@ int main(int argc, char* argv[])
 	  finisherror(con);
 
 	printf("MySQL client version: %s\n\n", mysql_get_client_info());
-	//sleep(5);
       }
 
 	return(0);
